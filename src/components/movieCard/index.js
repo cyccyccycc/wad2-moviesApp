4c13075bd3f154c1126b3788757ca571949772d1
YiCheng Cai <20091598@mail.wit.ie>
2020-11-02 12:00:18 +0000

    Add to favorites logic completed; Dummy favorites page.

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -4,22 +4,17 @@ import "./movieCard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../../globals/fontawesome";
 
-const MovieCard = props => {
+const MovieCard = (props) => {
+
+  const handleAddToFavorites = e => {
+    e.preventDefault()
+    props.buttonHandler(props.movie.id)  // new line
+  }
   return (
      
     <div className="col-sm-3">
       <div className="card  bg-white">
-        <img
-          className="card-img-tag center "
-          alt={props.movie.title}
-          src={
-            props.movie.poster_path
-              ? `https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`
-              : "./film-poster-placeholder.png"
-          }
-        />
-        <div className="card-body">
-        <Link to={`/movies/${props.movie.id}`}>
+      <Link to={`/movies/${props.movie.id}`}>
     <img
       className="card-img-tag center "
       alt={props.movie.title}
@@ -30,18 +25,22 @@ const MovieCard = props => {
       }
     />
   </Link>
+        <div className="card-body">
           <h4 className="card-title ">{props.movie.title}</h4>
           <p>
             <FontAwesomeIcon icon={["fas", "calendar"]} />
             <span> {props.movie.release_date}</span>
           </p>
+          
           <p>
             <FontAwesomeIcon icon={["fas", "star"]} />
             <span> {props.movie.vote_average}</span>
           </p>
         </div>
         <div className="card-footer">
-          <button type="button" className="btn w-100 btn-primary">
+        <button type="button" className="btn w-100 btn-primary"
+                onClick={handleAddToFavorites}
+                >
             Add to Favorites
           </button>
         </div>
