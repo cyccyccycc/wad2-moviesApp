439c1d3d0d4b892199d5cb634a62e186c052680c
YiCheng Cai <20091598@mail.wit.ie>
2020-11-04 16:11:30 +0000

    E2E tests - Navigation - Movies Details page Back button

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -76,4 +76,23 @@ describe("Navigation", () => {
       cy.get("h2").contains(movies[0].title);
     });
   });
+  describe("The Go Back button", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
+    it("should navigate from home page to movie details and back", () => {
+      cy.get(".card").eq(1).find("img").click();
+      cy.get("svg[data-icon=arrow-circle-left]").click();
+      cy.url().should("not.include", `/movies`);
+      cy.get("h2").contains("Discover Movies");
+    });
+    it("should navigate from favorites page to movie details and back", () => {
+        cy.contains("Add to Favorites").click();
+        cy.contains("Favorites").click();
+        cy.get(".card").eq(0).find("img").click();
+        cy.get("svg[data-icon=arrow-circle-left]").click();
+        cy.url().should("include", `/movies/favorites`);
+        cy.get("h2").contains("Favorite Movies");
+    });
+  });
 });
